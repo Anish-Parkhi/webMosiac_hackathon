77924878ba615a305d7c4a463986b4b2e15e88ee
Anish-Parkhi <anishparkhi03@gmail.com>
2024-01-27 18:31:00 +0530

    added the navbar container

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Navabar from '../Navbar/Navbar';
+import Sidebar from '../Sidebar/Sidebar';
+import styles from './Home.module.css';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.homeWrapperContainer}>
+      <div className={styles.homeMainContainer}>
+        <Navabar />
+        <div>Hello</div>
+        {/* rest code of home  */}
+      </div>
+      <div className={styles.sidebarWrapperContainer}>
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './Navbar.module.css';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className={styles.navbarMainContainer}>
+      <ul className={styles.navbarListContainer}>
+        <li className={styles.navbarLogo}>Blog Spot</li>
+        <li>Home</li>
+        <li>Top Blogs</li>
+        <li>Favourite</li>
+        <li>Category</li>
+        <li className={styles.accountLogoContainer}>A</li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
